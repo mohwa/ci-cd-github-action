5da1c6703a2a28dda9be469319da39bc24d06d80
mohwa <yanione2@gmail.com>
2022-08-01 10:36:39 +0900

    Add verify.sh

---
+++ server/hack/verify.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+find_files() {
+  find . -name '*.go'
+}
+
+diff=$(find_files | xargs gofmt -d -s 2>&1) || true
+if [[ -n "${diff}" ]]; then
+  echo "${diff}" >&2
+  echo >&2
+  echo "Run ./update.sh" >&2
+  exit 1
+fi
+
+diff=$(find_files | xargs goimports -d 2>&1) || true
+if [[ -n "${diff}" ]]; then
+  echo "${diff}" >&2
+  echo >&2
+  echo "Run ./update.sh" >&2
+  exit 1
+fi
+
+go mod tidy
+diff=$(git diff -- go.mod go.sum 2>&1) || true
+if [[ -n "${diff}" ]]; then
+  echo "${diff}" >&2
+  echo >&2
+  echo "Run ./update.sh and commit diff" >&2
+  exit 1
+fi
+
+diff=$(git diff -- api/graphql 2>&1) || true
+if [[ -n "${diff}" ]]; then
+  echo "${diff}" >&2
+  echo >&2
+  echo "Run 'go generate gqlgen.go' and commit diff" >&2
+  exit 1
+fi
